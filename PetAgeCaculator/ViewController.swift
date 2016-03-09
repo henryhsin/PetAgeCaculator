d36f2a4ea91b20447a9d8c94dc06283da6e652d7
辛忠翰 <xinzhonghan@xinzhonghande-macbook-pro.local>
2016-03-10 03:32:38 +0800

    Initial Commit

---
+++ PetAgeCaculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PetAgeCaculator
+//
+//  Created by 辛忠翰 on 2016/3/10.
+//  Copyright © 2016年 Hsinhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
